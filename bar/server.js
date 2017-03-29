3afa8484c182a143cf5b6485f6cc9362427f9446
kusigit <m.fuerer@vtxmail.ch>
2017-03-29 14:59:55 +0200

    Update server.js

--- bar/server.js
+++ bar/server.js
@@ -1,14 +1,10 @@
-// Load the http module to create an http server.
-var http = require('http');
+var express = require('express');
+var app = express();
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello bar\n");
-});
+app.use(express.static(__dirname + '/'));
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8080);
+// app.get('/bar', function(req, res){  
+ // res.send('hello world');
+//});
 
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8080/");
+app.listen(3000);
